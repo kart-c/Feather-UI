0c6a7a08a842836ea5ba7c5d3773e9a5e6a5e7f6
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-02-20 19:20:42 +0530

    feat: add modal functionality

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//  JS for backdrop and mobile menu
 const backdrop = document.querySelector('.backdrop');
 const navList = document.querySelector('.header-nav-list');
 const aside = document.querySelector('aside');
@@ -30,3 +31,46 @@ const backdropClickHandler = () => {
 mobileMenu.addEventListener('click', menuClickHandler);
 
 backdrop.addEventListener('click', backdropClickHandler);
+
+// JS for opening Modal
+
+const simpleModal = document.querySelector('.modal');
+const modalExampleBtn = document.querySelector('.modal-example > button');
+const modalBackdrop = document.querySelector('.modal-backdrop');
+const modalCloseBtn = document.querySelector('.modal-header > i');
+const actionModal = document.querySelectorAll('.modal')[1];
+const modalOpener = document.querySelectorAll('.modal-example > button')[1];
+const actionModalBackdrop = document.querySelector('.modal-backdrop.action-modal');
+const actionModalCloseBtn = document.querySelector('.action-modal .modal-header > i');
+
+const simpleModalHandler = () => {
+	modalBackdrop.classList.remove('modal-hide');
+	simpleModal.classList.remove('modal-hide');
+};
+
+const closeModal = () => {
+	simpleModal.classList.add('modal-hide');
+	modalBackdrop.classList.add('modal-hide');
+};
+
+modalExampleBtn.addEventListener('click', simpleModalHandler);
+
+modalBackdrop.addEventListener('click', closeModal);
+
+modalCloseBtn.addEventListener('click', closeModal);
+
+const actionModalHandler = () => {
+	actionModalBackdrop.classList.remove('modal-hide');
+	actionModal.classList.remove('modal-hide');
+};
+
+const closeActionModal = () => {
+	actionModalBackdrop.classList.add('modal-hide');
+	actionModal.classList.add('modal-hide');
+};
+
+modalOpener.addEventListener('click', actionModalHandler);
+
+actionModalBackdrop.addEventListener('click', closeActionModal);
+
+actionModalCloseBtn.addEventListener('click', closeActionModal);
